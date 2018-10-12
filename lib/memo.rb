c340778e9e5a63f0c34fa78da2e4974434761c5c
igor <ig.plotnikov@gmail.com>
2018-10-12 17:02:40 +0300

    Add read_from_console to memo.rb

--- lib/memo.rb
+++ lib/memo.rb
@@ -1,7 +1,17 @@
 class Memo < Post
 
   def read_from_console
+    puts "Новая заметка (Для выхода наберите end)"
 
+    @text = []
+
+    while line != "end"
+      line = STDIN.gets.chomp
+
+      @text << line
+    end
+
+    @text.pop
   end
 
   def to_string
