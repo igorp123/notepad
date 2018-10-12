d3e34769f68e2f825085312c19230865f014fb5d
igor <ig.plotnikov@gmail.com>
2018-10-12 17:07:14 +0300

    Add read_from_console to link.rb

--- lib/link.rb
+++ lib/link.rb
@@ -6,7 +6,11 @@ class Link < Post
   end
 
   def read_from_console
+    puts "Адресс ссылки:"
+    @url = STDIN.gets.chomp
 
+    puts "Что за ссылка"
+    @text = STDIN.gets.chomp
   end
 
   def to_string
